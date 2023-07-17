fb0556a2613b779c353c14d43e46314c7bb17a52
panziang <2385863274@qq.com>
2023-07-17 18:32:17 +0800

    feat: 新增jsx以及tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,11 +7,8 @@ const count = ref(0)
 <template>
   <div class="card">
     <button @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
+  <h1 class="text-3xl font-bold underline">giao</h1>
 </template>
 
 <style scoped>
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,1 +1,57 @@
-export default () => <div>test</div>
+//1. 函数式组件
+
+import { defineComponent, ref, withModifiers } from 'vue'
+
+// export default () => <div>test</div>
+
+// 2. defineComponent
+// export default defineComponent({
+//   render() {
+//     return <div>tes1111t</div>
+//   }
+// })
+
+// 3. defineComponent({setup{}})
+export default defineComponent({
+  //自定义指令
+  directives: {
+    focus: {
+      mounted(el) {
+        el.focus()
+      }
+    }
+  },
+  // 事件派发
+  emits: ['click'],
+  setup(props, { slots, emit }) {
+    const count = ref(0)
+
+    const inc = () => {
+      count.value++
+      //向外界派发事件
+      emit('click')
+    }
+    const list = ref<string[]>(['a', 'b', 'c'])
+    return () => {
+      //与视图相关
+      const span = count.value ? <span>A</span> : <span>B</span>
+      return (
+        <div onClick={withModifiers(inc, ['self'])}>
+          test{count.value}
+          <input type="text" v-focus v-model={count.value} />
+          <div>{span}</div>
+          <ul>
+            {list.value.map(str => (
+              <li key={str}>{str}</li>
+            ))}
+          </ul>
+          {/* 默认插槽内容 */}
+          <div>{slots.default ? slots.default() : 'default content'}</div>
+          {/* 具名插槽 */}
+          <div>{slots.title ? slots.title() : 'title slot  content'}</div>
+          {/* <Test v-slots={{ title: () => <h3>title</h3> }}>abc</Test> */}
+        </div>
+      )
+    }
+  }
+})
