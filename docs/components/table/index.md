367008e2969ce9d4006f8519f3db92756b775d5d
panziang <2385863274@qq.com>
2023-07-28 19:57:29 +0800

    feat: 新增table组件勾选以及事件处理

--- docs/components/table/index.md
+++ docs/components/table/index.md
@@ -1,13 +1,21 @@
-# 按钮 -Button
+# 表格 -Table
 
 :::demo 这是Table组件
 
 ``` vue
 <template>
-  <z-table :data="tableData">
+  <z-table :data="tableData" @selection-change="onSelectionChange">
+    <z-column type="selection"/>
     <z-column field="date" header="Date" />
     <z-column field="name" header="Name" />
     <z-column field="address" header="Address" />
+    <z-column  header="操作" >
+      <template #default="row">
+        <button @click=editRow(row)>编辑</button>
+        <button>删除</button>
+      </template>
+
+    </z-column>
 
   </z-table>
 </template>
@@ -17,21 +25,30 @@ import { ref } from 'vue';
 const tableData = ref([
   {
     date: '2016-05-03',
-    name: 'Mark',
+    name: 'Mark1',
     address: 'No. 189, Grove St, Los Angeles',
     checked: true
   },
   {
     date: '2016-05-03',
-    name: 'Mark',
-    address: 'No. 189, Grove St, Los Angeles'
+    name: 'Mark2',
+    address: 'No. 189, Grove St, Los Angeles',
+    checked: true
   },
   {
     date: '2016-05-03',
-    name: 'Mark',
+    name: 'Mark3',
     address: 'No. 189, Grove St, Los Angeles'
   }
 ])
+
+const editRow = (row) => {
+  console.log('editRow', row)
+}
+
+const onSelectionChange = (checkedRows) => {
+  console.log('checkedRows', checkedRows)
+}
 
 </script>
 
--- src/table/src/table.tsx
+++ src/table/src/table.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, provide, ref, toRefs } from 'vue'
+import { defineComponent, provide, ref, toRefs, watch } from 'vue'
 import { TableProps, tableProps } from './table-type'
 import '../style/table.scss'
 import { ColumnContext } from './column-type'
@@ -6,12 +6,52 @@ import { ColumnContext } from './column-type'
 export default defineComponent({
   name: 'ZTable',
   props: tableProps,
-  setup(props: TableProps, { slots }) {
+  emits: ['selection-change'],
+  setup(props: TableProps, { slots, emit }) {
     const { data } = toRefs(props)
 
     //获取Column组件中的列数据用于td
     const columnData = ref([])
     provide('column-data', columnData)
+
+    //check变化事件处理
+    watch(
+      data,
+      (newData: any) => {
+        const checkedRows = newData.filter((row: any) => row.checked)
+        if (checkedRows.length === data.value.length) {
+          //全选
+          allChecked.value = true
+          isIndeterminate.value = false
+        } else if (checkedRows.length === 0) {
+          //全不选
+          allChecked.value = false
+          isIndeterminate.value = false
+        } else {
+          //半选
+          isIndeterminate.value = true
+        }
+        emit('selection-change', checkedRows)
+      },
+      //深度监听
+      { deep: true }
+    )
+
+    //提供给column的全选状态值
+    const allChecked = ref(data.value.every((row: any) => row.checked))
+    provide('all-checked', allChecked)
+    watch(allChecked, newVal => {
+      data.value.forEach((row: any) => {
+        row.checked = newVal
+      })
+    })
+
+    //半选状态值
+    //有值被选中且不是全选
+    const isIndeterminate = ref(
+      data.value.some((row: any) => row.checked && !allChecked.value)
+    )
+    provide('is-indeterminate', isIndeterminate)
 
     return () => {
       return (
@@ -23,9 +63,27 @@ export default defineComponent({
             {data.value.map((row: any) => {
               return (
                 <tr>
-                  {columnData.value.map((column: ColumnContext) => {
-                    return column.field ? <td>{row[column.field!]}</td> : null
-                  })}
+                  {columnData.value.map(
+                    (column: ColumnContext, index: number) => {
+                      // 如果存在默认插槽，优先渲染
+                      const columnSlots = slots.default?.()[index]
+                      if (columnSlots?.children) {
+                        return (
+                          <td>
+                            {(columnSlots?.children as any).default?.(row)}
+                          </td>
+                        )
+                      }
+                      //没有插槽
+                      return column.field ? (
+                        <td>{row[column.field]}</td>
+                      ) : column.type === 'selection' ? (
+                        <input type="checkbox" v-model={row.checked} />
+                      ) : (
+                        ''
+                      )
+                    }
+                  )}
                 </tr>
               )
             })}
